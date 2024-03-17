e4224c4cda47248f6fb8a2676828d48f78592b7e
Jorge Oteiza <oteiza.jor@gmail.com>
2024-03-17 22:28:02 +0000

    estructura e instalaciones.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/style.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todoListData, setTodoListData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const printList = () => {
+    return todoListData.length > 0 ? (
+      todoListData.map((todoText, index) => (
+        <article key={index}>
+          <p
+            onMouseEnter={(e) =>
+              (e.target.querySelector("button").style.display = "inline-block")
+            }
+            onMouseLeave={(e) =>
+              (e.target.querySelector("button").style.display = "none")
+            }
+          >
+            <span>{todoText}</span>{" "}
+            <button onClick={() => deleteTodoItem(todoText)}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </p>
+        </article>
+      ))
+    ) : (
+      <p
+        style={{
+          color: "red",
+          fontStyle: "italic",
+          fontSize: "18px",
+          textAlign: "center",
+        }}
+      >
+        La lista está vacía
+      </p>
+    );
+  };
+
+  const addTodoItem = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) return;
+    setTodoListData([...todoListData, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteTodoItem = (todoItemToDelete) => {
+    setTodoListData(todoListData.filter((value) => value !== todoItemToDelete));
+  };
+
+  return (
+    <div>
+      <h1>LISTA</h1>
+      <form onSubmit={addTodoItem}>
+        <input
+          name="todoInput"
+          id="todoInput"
+          placeholder="Agregar nuevo"
+          autoFocus
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </form>
+      <section id="todoList">{printList()}</section>
+    </div>
+  );
 };
 
 export default Home;
